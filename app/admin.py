a350d05ec864cb8f626aec0a900a8aae7856d152
Mathias Gonzalez <53032987+sc2-zobka@users.noreply.github.com>
2020-10-29 21:53:54 -0300

    Feature/slider model (#1)
    * [ADD] Slider model:
    
    - Model Slider added to django admin
    - Display components on Slider-admin:
    	1. column's header on list_view
    	2. search box tool
    	3. filter tool
    	4. paginator tool
    
    * [ADD] Slider images:
    
    Slider images now can be modified from django admin interface.

--- app/admin.py
+++ app/admin.py
@@ -1,25 +1,37 @@
 from django.contrib import admin
-from .models import Servicio #Insumo
+from .models import Servicio, Slider  # Insumo
 
 # Register your models here.
 
 
 class ServicioAdmin(admin.ModelAdmin):
     '''
-        Display components on "Servicio" section
+        Display components on "Servicio" admin section
         based on Servicio model's fields
     '''
     # display column's header on list_view
     list_display = ["nombre", "precio", "descripcion"]
-    # display a search tool
+    # display a search box tool
     search_fields = ["nombre"]
     # display a filter tool
     list_filter = ["nombre"]
     # display paginator at the bottom of the list_view
     list_per_page = 1
-    
+
+
+class SilderAdmin(admin.ModelAdmin):
+    '''
+        Display components on "Slider" admin section
+        based on Slider model's fields
+    '''
+    list_display = ["nombre", "descripcion", "imagen"]
+    search_fields = ["nombre", "imagen"]
+    list_filter = ["nombre", "imagen"]
+    list_per_page = 1
+
 
 admin.site.register(Servicio, ServicioAdmin)
+admin.site.register(Slider, SilderAdmin)
 
 # change admin title
 admin.site.site_header = "Administración Car Wash"
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Servicio
+from .models import Servicio, Slider
 
 # Create your views here.
 
@@ -8,10 +8,13 @@ def home(request):
     # fetch all Servicios objects into a list[]
     servicios = Servicio.objects.all()
 
+    fotos_slider = Slider.objects.all()
+
     # store into data{} all Servicio's objects
     # and render it to django template "Home"
     data = {
         'servicios': servicios,  # key:'servicios' could be any name
+        'slider': fotos_slider
     }
     return render(request, 'app/home.html', data)  # data as a third param
 
